d6456edfd3d1e8bd93c7c6b1f7638564e0a0616f
albegon1 <albe7369@colorado.edu>
2018-11-10 03:21:11 -0700

    starting e3

---
+++ source_e3.R
@@ -0,0 +1,18 @@
+# Sourcing libraries, data, and functions
+
+##
+## Source Libraries
+##
+libr=c("magrittr","maps","akima","fields")
+options(warn=-1)
+suppressPackageStartupMessages(lapply(libr, require,character.only = TRUE))
+
+
+##
+## Source data
+##
+
+
+##
+## Source functions
+## 
